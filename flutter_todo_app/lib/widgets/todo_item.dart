c91127a4d9eb6f31a9a7ecab55a53e1c51f62a23
Gustavo Lopes <gustavo.figueira@altbank.co>
2022-10-24 11:04:34 -0300

    fix

--- flutter_todo_app/lib/widgets/todo_item.dart
+++ flutter_todo_app/lib/widgets/todo_item.dart
@@ -21,13 +21,13 @@ class ToDoItem extends StatelessWidget {
       margin: const EdgeInsets.only(bottom: 20),
       child: ListTile(
         onTap: () {
-          // print('Clicked on Todo Item.');
-          emAlterarToDo(todo);
+          print('Clicou');
+          //emAlterarToDo(todo);
         },
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Icon(
           todo.feito ? Icons.check_box : Icons.check_box_outline_blank,
@@ -42,20 +42,20 @@ class ToDoItem extends StatelessWidget {
           ),
         ),
         trailing: Container(
-          padding: EdgeInsets.all(0),
-          margin: EdgeInsets.symmetric(vertical: 12),
-          height: 35,
-          width: 35,
+          padding: const EdgeInsets.all(0),
+          margin: const EdgeInsets.symmetric(vertical: 5),
+          height: 40,
+          width: 40,
           decoration: BoxDecoration(
             color: tdRed,
             borderRadius: BorderRadius.circular(5),
           ),
           child: IconButton(
             color: Colors.white,
-            iconSize: 18,
-            icon: Icon(Icons.delete),
+            iconSize: 16,
+            icon: const Icon(Icons.delete),
             onPressed: () {
-              // print('Clicked on delete icon');
+              // print('Excluiu');
               emDeletarItem(todo.id);
             },
           ),
